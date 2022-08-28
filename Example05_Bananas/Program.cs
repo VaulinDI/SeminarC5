39227f883501cbca0394eb2e8d527b9a968c8c0c
VaulinDI <vdi85@mail.ru>
2022-08-28 13:43:11 +0300

    bananas task solved

---
+++ Example05_Bananas/Program.cs
@@ -0,0 +1,11 @@
+﻿int k = 3;
+int n = 17;
+int w = 4;
+int sumprice = 0;
+
+for (int i = 1; i <= w; i++)
+{
+    sumprice = sumprice + k * i;
+}
+Console.WriteLine($"Общая стоимость бананов: {sumprice} долларов");
+Console.WriteLine($"Солдату придется одолжить: {sumprice - n} долларов");
