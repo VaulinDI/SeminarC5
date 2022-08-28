eba2b69e5323ab4748a71f39267f441a3c7949be
VaulinDI <vdi85@mail.ru>
2022-08-28 13:23:59 +0300

    happynumber task solved

---
+++ Example04_HappyNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿long num = 107407450;
+Console.WriteLine(num);
+long x = 0;
+int count = 0;
+
+for (long i = 1; i < num; i = i * 10)
+{
+    x = num / i % 10;
+    if (x == 4 || x == 7)
+    {
+        count++;
+    }
+}
+
+if (count == 4 || count == 7)
+{
+    Console.WriteLine("Число является почти счастливым");
+}
+else
+{
+    Console.WriteLine("Число НЕ является почти счастливым");
+}
